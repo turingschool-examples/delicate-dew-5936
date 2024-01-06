945032941c34f92406d488654c1c1893d35d67a0
Nash <141100397+s2an@users.noreply.github.com>
2024-01-06 14:49:18 -0500

    Adds create to mechanic controller and gets stuck on creating the form

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,4 +2,13 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
+  
+  def create 
+    @mechanic = Mechanic.new(mechanic_params)
+    # if @mechanic.save
+    #   redirect_to @mechanic
+    # else
+    #   render :new
+    # end
+  end
 end
