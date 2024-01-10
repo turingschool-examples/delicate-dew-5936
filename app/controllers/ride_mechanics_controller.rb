affa00f543d89d081831cac434da74b1828ca0b4
Quin Nordmark <quinnordmark@gmail.com>
2024-01-09 20:16:19 -0700

    us2 done i feel so stupid

--- app/controllers/ride_mechanics_controller.rb
+++ app/controllers/ride_mechanics_controller.rb
@@ -1,7 +1,7 @@
 class RideMechanicsController < ApplicationController 
     def create 
-        mechanic = Mechanic.find(params[:id])
-        ride = Ride.find(params[:id])
+        mechanic = Mechanic.find(params[:mechanic_id])
+        ride = Ride.find(params[:ride_id])
 
         ride.mechanics << mechanic
         mechanic.rides << ride
